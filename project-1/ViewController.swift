0c6c1198b7b25bcfb02e2359d6631a6607f46ed0
Melek <53395371+m-333@users.noreply.github.com>
2022-10-12 23:36:12 +0300

    Initial Commit

---
+++ project-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-1
+//
+//  Created by melek türüdi on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
