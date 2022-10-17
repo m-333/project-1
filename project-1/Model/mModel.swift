5fe036d94d01e13bd0347fa00501fb6a1607e8e6
Melek <53395371+m-333@users.noreply.github.com>
2022-10-17 23:51:28 +0300

    story module added

---
+++ project-1/Model/mModel.swift
@@ -0,0 +1,32 @@
+//
+//  mModel.swift
+//  project-1
+//
+//  Created by melek türüdi on 14.10.2022.
+//
+
+import Foundation
+struct UserDetails {
+    var name: String = ""
+    var imageUrl: String = ""
+    var content: [Content] = []
+    
+    init(userDetails: [String: Any]) {
+        name = userDetails["name"] as? String ?? ""
+        imageUrl = userDetails["imageUrl"] as? String ?? ""
+        let aContent = userDetails["content"] as? [[String : Any]] ?? []
+        for element in aContent {
+            content += [Content(element: element)]
+        }
+    }
+}
+
+struct Content {
+    var type: String
+    var url: String
+    init(element: [String: Any]) {
+        type = element["type"] as? String ?? ""
+        url = element["url"] as? String ?? ""
+    }
+}
+
---
+++ project-1/view/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  project-1
+//
+//  Created by melek türüdi on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usersCollectionView: UICollectionView!
+    
+    var userDetails: [UserDetails] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        fetchUserData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Private func
+    private func fetchUserData() {
+        let path = Bundle.main.path(forResource: "user-details", ofType: "json")
+        let data = NSData(contentsOfFile: path ?? "") as Data?
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            if let aUserDetails = json["userDetails"] as? [[String : Any]] {
+                for element in aUserDetails {
+                    userDetails += [UserDetails(userDetails: element)]
+                }
+            }
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    // MARK: - UICollectionViewDataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return userDetails.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! usersCollectionViewCell
+        cell.imgView.imageFromServerURL(userDetails[indexPath.row].imageUrl)
+        cell.lblUserName.text = userDetails[indexPath.row].name
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        DispatchQueue.main.async {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "ContentView") as! ContentViewController
+            vc.modalPresentationStyle = .overFullScreen
+            vc.pages = self.userDetails
+            vc.currentIndex = indexPath.row
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+}
