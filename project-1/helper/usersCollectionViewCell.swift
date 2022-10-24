6124a04c4513fc0ba63fda070ea116e5d7248d08
Melek <53395371+m-333@users.noreply.github.com>
2022-10-24 19:07:31 +0300

    change

--- project-1/helper/usersCollectionViewCell.swift
+++ project-1/helper/usersCollectionViewCell.swift
@@ -14,10 +14,13 @@ class usersCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.imgView.layer.borderColor = UIColor.blue.cgColor
+        
+        
         self.lblUserName.textAlignment = .center
         self.imgView.layer.cornerRadius = self.imgView.frame.size.height / 2;
-        self.imgView.layer.borderColor = UIColor.red.cgColor
         self.imgView.layer.borderWidth = 3
         self.imgView.clipsToBounds = true
     }
+   
 }
